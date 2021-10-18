80d9b69251ad525fd801f57941c85f9575de40a6
dazesace <dazemcblaze@gmail.com>
2021-10-18 15:30:46 +0200

    some chill ui with buttons n icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import Navbar from "./components/Navbar";
+import {IoMdFlame} from "react-icons/io"
+import {MdOutlineComputer, MdBiotech} from "react-icons/md"
+import {HiPlus} from "react-icons/hi"
+
 function App() {
     return (
-        <div className="flex justify-center w-3/4 mx-auto bg-red-300 py-6">
-            <p className={"text-2xl font-bold"}>EasyTicket</p>
+        <div className="flex flex-col justify-center">
+            <Navbar/>
+            <div
+                className={"body"}>
+                <p className={"head"}>Womit haben Sie ein Problem?</p>
+                <div className={"buttongrid"}>
+                    <div
+                        className={"button"}>
+                        <p className={"text-center"}><MdOutlineComputer className={"inline h-16 w-16"}/></p>
+                        <p className={"text-xl font-semibold"}>Computer</p>
+                    </div>
+                    <div
+                        className={"button"}>
+                        <p className={"text-center"}><IoMdFlame className={"inline h-16 w-16"}/></p>
+                        <p className={"text-xl font-semibold"}>Heizung</p>
+                    </div>
+                    <div
+                        className={"button"}>
+                        <p className={"text-center"}><MdBiotech className={"inline h-16 w-16"}/></p>
+                        <p className={"text-xl font-semibold"}>Medizintechnik</p>
+                    </div>
+                    <div
+                        className={"button"}>
+                        <p className={"text-center"}><HiPlus className={"inline h-16 w-16"}/></p>
+                        <p className={"text-xl font-semibold"}>Sonstiges</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className={"flex w-full bg-gray-900"}>
+            <div className={"flex w-11/12 mx-auto py-4"}>
+                <p className={"text-2xl text-green-50 font-bold"}>EasyTicket</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
