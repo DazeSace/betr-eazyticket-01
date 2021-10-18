2ebf2e286f3090946c1a75d8d0747c8744bcec31
dazesace <dazemcblaze@gmail.com>
2021-10-18 11:32:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex justify-center w-3/4 mx-auto bg-red-300 py-6">
+            <p className={"text-2xl font-bold"}>EasyTicket</p>
+        </div>
+    );
 }
 
 export default App;
